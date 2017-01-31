e1bc69b2c219de9a672963bc37a3abff58f87dc8
James Smith <jismith1989@googlemail.com>
2017-01-31 14:27:12 +0000

    Include package documentation.

---
+++ package_doc.go
@@ -0,0 +1,5 @@
+// Package provides a constructor function for a pointer to a
+// type (environment) that can read, parse and store environment
+// files, strings and variables (with support for mutexes).
+package envStore
+
