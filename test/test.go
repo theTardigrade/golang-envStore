abaeb33ba8395fef9c26f503d3a6210fd6186546
James Smith <jismith1989@googlemail.com>
2017-01-31 18:35:32 +0000

    Invert conditional in AssertEqual function of test package.

--- test/test.go
+++ test/test.go
@@ -6,7 +6,10 @@ import (
 )
 
 func AssertEqual(t *testing.T, name string, expectedValue, actualValue interface{}) {
-	if !reflect.DeepEqual(expectedValue, actualValue) {
-		t.Errorf("Expected %s to be \"%s\", but got \"%s\".", name, expectedValue, actualValue)
+	if reflect.DeepEqual(expectedValue, actualValue) {
+		return
+	}
+
+	t.Errorf("Expected %s to be \"%s\", but got \"%s\".", name, expectedValue, actualValue)
 	}
 }
