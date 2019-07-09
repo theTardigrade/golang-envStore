573787227ba0dad38f0242ae46ee5d6465496502
James <jismith1989@googlemail.com>
2019-07-09 20:26:25 +0100

    Accept pointer to Config in new Function

--- constructor.go
+++ constructor.go
@@ -4,7 +4,7 @@ import (
 	"sync"
 )
 
-func New(cfg Config) (*Environment, error) {
+func New(cfg *Config) (*Environment, error) {
 	env := &Environment{
 		data:             make(dictionary),
 		useMutex:         cfg.UseMutex,
@@ -13,45 +13,47 @@ func New(cfg Config) (*Environment, error) {
 		maxKeyLength:     cfg.MaxKeyLength,
 	}
 
-	if cfg.UseMutex {
-		env.mutex = &sync.RWMutex{}
-	}
+	if cfg != nil {
+		if cfg.UseMutex {
+			env.mutex = &sync.RWMutex{}
+		}
 
-	if cfg.FromFilePaths != nil {
-		for _, path := range cfg.FromFilePaths {
-			if err := env.LoadFromFile(path); err != nil {
-				return nil, err
+		if cfg.FromFilePaths != nil {
+			for _, path := range cfg.FromFilePaths {
+				if err := env.LoadFromFile(path); err != nil {
+					return nil, err
+				}
 			}
 		}
-	}
 
-	if cfg.FromStrings != nil {
-		for _, str := range cfg.FromStrings {
-			if err := env.LoadFromString(str); err != nil {
-				return nil, err
+		if cfg.FromStrings != nil {
+			for _, str := range cfg.FromStrings {
+				if err := env.LoadFromString(str); err != nil {
+					return nil, err
+				}
 			}
 		}
-	}
 
-	if cfg.FromJSONSlices != nil {
-		for _, data := range cfg.FromJSONSlices {
-			if err := env.LoadFromJSON(data); err != nil {
-				return nil, err
+		if cfg.FromJSONSlices != nil {
+			for _, data := range cfg.FromJSONSlices {
+				if err := env.LoadFromJSON(data); err != nil {
+					return nil, err
+				}
 			}
 		}
-	}
 
-	if cfg.FromEnvironments != nil {
-		for _, env2 := range cfg.FromEnvironments {
-			if err := env.LoadFromEnvironment(env2); err != nil {
-				return nil, err
+		if cfg.FromEnvironments != nil {
+			for _, env2 := range cfg.FromEnvironments {
+				if err := env.LoadFromEnvironment(env2); err != nil {
+					return nil, err
+				}
 			}
 		}
-	}
 
-	if cfg.FromSystem {
-		if err := env.LoadFromSystem(); err != nil {
-			return nil, err
+		if cfg.FromSystem {
+			if err := env.LoadFromSystem(); err != nil {
+				return nil, err
+			}
 		}
 	}
 
